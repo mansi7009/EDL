04b7a252b96bdee0af45ae57f93e4911a9328131
mansi <mansigangwani5645@gmail.com>
2022-08-04 18:42:16 +0530

    Initial commit

---
+++ src/dummydata.js
@@ -0,0 +1,418 @@
+export const homeAbout = [
+  {
+    id: 1,
+    cover: "https://img.icons8.com/dotty/80/000000/storytelling.png",
+    title: "Online Courses",
+    desc: "Online courses provides fantastic flexibility, among other benefits, and aids in making the task of arranging development opportunities far easier." ,
+  },
+  {
+    id: 1,
+    cover: "https://img.icons8.com/ios/80/000000/diploma.png",
+    title: "Earn A Certificates",
+    desc: "Get Certified; a certificate easily moves you at least thousand rows ahead of other job candidates, with similar knowledge.",
+  },
+  {
+    id: 1,
+    cover: "https://img.icons8.com/ios/80/000000/athlete.png",
+    title: "Learn with Expert",
+    desc: "Experts know what works and what doesn't work, which means they can help you generate better ideas.",
+  },
+]
+export const awrapper = [
+  {
+    cover: "https://img.icons8.com/external-yogi-aprelliyanto-basic-outline-yogi-aprelliyanto/80/ffffff/external-graduation-education-yogi-aprelliyanto-basic-outline-yogi-aprelliyanto.png",
+    data: "3,000",
+    title: "SUCCESS STORIES",
+  },
+  
+  {
+    cover: "https://img.icons8.com/ios/80/ffffff/athlete.png",
+    data: "320",
+    title: "TRUSTED TUTORS",
+  },
+  {
+    cover: "https://img.icons8.com/external-outline-icons-maxicons/80/ffffff/external-calender-insurance-outline-outline-icons-maxicons.png",
+    data: "1,000",
+    title: "SCHEDULES",
+  },
+  {
+    cover: "https://img.icons8.com/ios/80/ffffff/macbook-idea--v3.png",
+    data: "587",
+    title: "COURSES",
+  },
+]
+export const coursesCard = [
+  {
+    id: 1,
+    cover: "../images/courses/c1.png",
+    coursesName: "Introducing to Software Engineering and Designing",
+    courTeacher: [
+      {
+        dcover: "./images/back.webp",
+        name: "by John Smith",
+        totalTime: "50 lectures (190 hrs)",
+      },
+    ],
+    priceAll: "$100 All Course",
+    pricePer: "$15 per month",
+  },
+
+  {
+    id: 2,
+    cover: "../images/courses/c2.png",
+    coursesName: "Enhancing Adobe Photoshop CC 2020 Skills",
+    courTeacher: [
+      {
+        dcover: "./images/back.webp",
+        name: "by Ram Gurung",
+        totalTime: "30 lectures (125 hrs)",
+      },
+    ],
+    priceAll: "$200 All Course",
+    pricePer: "$25 per month",
+  },
+
+  // {
+  //   id: 3,
+  //   cover: "../images/courses/c3.png",
+  //   coursesName: "HTML, CSS, and Javascript for Web Developers",
+  //   courTeacher: [
+  //     {
+  //       dcover: "./images/back.webp",
+  //       name: "by Saroj Nepal",
+  //       totalTime: "50 lectures (50 hrs)",
+  //     },
+  //   ],
+  //   priceAll: "$50 All Course",
+  //   pricePer: "$5 per month",
+  // },
+
+  {
+    id: 3,
+    cover: "../images/courses/c4.png",
+    coursesName: "Introducing to Programming with WordPress",
+    courTeacher: [
+      {
+        dcover: "./images/back.webp",
+        name: "by Prabin Thapa Mager",
+        totalTime: "20 lectures (20 hrs)",
+      },
+    ],
+    priceAll: "$30 All Course",
+    pricePer: "$3 per month",
+  },
+  
+  {
+    id: 4,
+    cover: "../images/courses/c5.png",
+    coursesName: "Introduction to ReactJS and Angular JS",
+    courTeacher: [
+      {
+        dcover: "./images/back.webp",
+        name: "by Sunil Bishowkaram",
+        totalTime: "100 lectures (150 hrs)",
+      },
+    ],
+    priceAll: "$300 All Course",
+    pricePer: "$30 per month",
+  },
+  {
+    id: 6,
+    cover: "../images/courses/c6.png",
+    coursesName: "Learn Frontend Programming Language",
+    courTeacher: [
+      {
+        dcover: "./images/back.webp",
+        name: "by Sunil Bishowkaram",
+        totalTime: "200 lectures (300 hrs)",
+      },
+    ],
+    priceAll: "$500 All Course",
+    pricePer: "$80 per month",
+  },
+  {
+    id: 7,
+    cover: "../images/courses/c7.png",
+    coursesName: "Introducing to with HTML / CSS",
+    courTeacher: [
+      {
+        dcover: "./images/back.webp",
+        name: "by Price",
+        totalTime: "20 lectures (50 hrs)",
+      }, 
+    ],
+    priceAll: "$20 All Course",
+    pricePer: "$2 per month",
+  }
+  // {
+  //   id: 8,
+  //   cover: "../images/courses/c8.png",
+  //   coursesName: "Introducing to with JAVA",
+  //   courTeacher: [
+  //     {
+  //       dcover: "../images/projectImages/blog/pic.jpg",
+  //       name: "by Petter",
+  //       totalTime: "80 lectures (200 hrs)",
+  //     },
+  //   ],
+  //   priceAll: "$200 All Course",
+  //   pricePer: "$40 per month",
+  // },
+  // {
+  //   id: 9,
+  //   cover: "../images/courses/c9.png",
+  //   coursesName: "Introducing to with HTML / CSS / JAVASCRIPT",
+  //   courTeacher: [
+  //     {
+  //       dcover: "../images/projectImages/blog/pic.jpg",
+  //       name: "by Petter", 
+  //       totalTime: "50 lectures (50 hrs)",
+  //     },
+  //   ],
+  //   priceAll: "$20 All Course",
+  //   pricePer: "$3 per month",
+  // },
+]
+export const online = [
+  {
+    cover: "./images/courses/online/o1.png",
+    hoverCover: "./images/courses/online/o1.1.png",
+    courseName: "UI/UX Design Courses",
+    course: "25 Courses",
+  },
+  {
+    cover: "./images/courses/online/o2.png",
+    hoverCover: "./images/courses/online/o2.1.png",
+    courseName: "Art & Design",
+    course: "25 Courses",
+  },
+  {
+    cover: "./images/courses/online/o3.png",
+    hoverCover: "./images/courses/online/o3.1.png",
+    courseName: "Computer Science",
+    course: "10 Courses",
+  },
+  {
+    cover: "./images/courses/online/o4.png",
+    hoverCover: "./images/courses/online/o4.1.png",
+    courseName: "History & Archeologic",
+    course: "15 Courses",
+  },
+  {
+    cover: "./images/courses/online/o5.png",
+    hoverCover: "./images/courses/online/o5.1.png",
+    courseName: "Software Engineering",
+    course: "30 Courses",
+  },
+  {
+    cover: "./images/courses/online/o6.png",
+    hoverCover: "./images/courses/online/o6.1.png",
+    courseName: "Information Software",
+    course: "60 Courses",
+  },
+  {
+    cover: "./images/courses/online/o7.png",
+    hoverCover: "./images/courses/online/o7.1.png",
+    courseName: "Health & Fitness",
+    course: "10 Courses",
+  },
+  {
+    cover: "./images/courses/online/o8.png",
+    hoverCover: "./images/courses/online/o8.1.png",
+    courseName: "Marketing",
+    course: "30 Courses",
+  },
+  {
+    cover: "./images/courses/online/o9.png",
+    hoverCover: "./images/courses/online/o9.1.png",
+    courseName: "Graphic Design",
+    course: "80 Courses",
+  },
+  {
+    cover: "./images/courses/online/o10.png",
+    hoverCover: "./images/courses/online/o10.1.png",
+    courseName: "Music",
+    course: "120 Courses",
+  },
+  {
+    cover: "./images/courses/online/o11.png",
+    hoverCover: "./images/courses/online/o11.1.png",
+    courseName: "Business Administration",
+    course: "17 Courses",
+  },
+  {
+    cover: "./images/courses/online/o12.png",
+    hoverCover: "./images/courses/online/o12.1.png",
+    courseName: "Web Management",
+    course: "17 Courses",
+  },
+]
+export const team = [
+  {
+    cover: "../images/team/t1.webp",
+    name: "Mr. Ian Somerhalder",
+    work: "DEVELOPER AND LEAD INSTRUCTOR",
+  },
+  {
+    cover: "../images/team/t2.webp",
+    name: "Chris Evans",
+    work: "DEVELOPER AND LEAD INSTRUCTOR",
+  },
+  {
+    cover: "../images/team/t3.webp",
+    name: "Monica Geller",
+    work: "DEVELOPER AND LEAD INSTRUCTOR",
+  },
+  {
+    cover: "../images/team/t4.webp",
+    name: "Shwan Mendes",
+    work: "DEVELOPER AND LEAD INSTRUCTOR",
+  },
+  // {
+  //   cover: "../images/team/t5.webp",
+  //   name: "Ph.D Adrian Molises",
+  //   work: "DEVELOPER AND LEAD INSTRUCTOR",
+  // },
+  // {
+  //   cover: "../images/team/t6.webp",
+  //   name: "Ph.D Arthur MaGregor",
+  //   work: "DEVELOPER AND LEAD INSTRUCTOR",
+  // },
+  // {
+  //   cover: "../images/team/t7.webp",
+  //   name: "Ph.D Anna Hanzen",
+  //   work: "DEVELOPER AND LEAD INSTRUCTOR",
+  // },
+  // {
+  //   cover: "../images/team/t8.webp",
+  //   name: "Ph.D Brian Wooden",
+  //   work: "DEVELOPER AND LEAD INSTRUCTOR",
+  // },
+]
+export const price = [
+  {
+    name: "BASIC PLAN",
+    price: "10K",
+    desc: "With EDL for Campus Basic plan, students receive free access to Guided Projects—hands-on, expert-led opportunities to quickly learn applied skills.",
+  },
+  {
+    name: "BEGINNER PLAN",
+    price: "17K",
+    desc: "A training session plan  also called a learning plan is an organized description of the activities and resources you'll use to guide a group toward a specific learning objective.",
+  },
+  {
+    name: "PREMIUM PLAN",
+    price: "25k",
+    desc: "As we wish to provide the best for you, we have come up with more features on our website to give you the best user experience possible. website to give you the best user" ,
+  },
+  {
+    name: "ULTIMATE PLAN",
+    price: "35K",
+    desc: "ULTIMATE Premium EDL has been introduced to provide you with quality content and features at a minimal price! website to give you the best user experience possible.",
+  },
+]
+export const faq = [
+  {
+    title: "How to Enroll This Online Courses?",
+    desc: "This is the first item's accordion body. It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. It's also worth noting that just about any HTML can go within the .accordion-body, though the transition does limit overflow.",
+  },
+  {
+    title: "Where It is hidden by default, until the collapse?",
+    desc: "It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. It's also worth noting that just about any HTML can go within the .accordion-body, though the transition does limit overflow.",
+  },
+  {
+    title: "How It is hidden by default, until the collapse?",
+    desc: "It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. It's also worth noting that just about any HTML can go within the .accordion-body, though the transition does limit overflow.",
+  },
+  {
+    title: "How to Enroll This Online Courses?",
+    desc: "This is the first item's accordion body. It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. It's also worth noting that just about any HTML can go within the .accordion-body, though the transition does limit overflow.",
+  },
+  {
+    title: "Where It is hidden by default, until the collapse?",
+    desc: "It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. It's also worth noting that just about any HTML can go within the .accordion-body, though the transition does limit overflow.",
+  },
+  {
+    title: "How It is hidden by default, until the collapse?",
+    desc: "It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. It's also worth noting that just about any HTML can go within the .accordion-body, though the transition does limit overflow.",
+  },
+]
+export const blog = [
+  {
+    id: 1,
+    type: "admin",
+    date: "JAN. 18, 2021",
+    com: "3 COMMENTS ",
+    title: "Build my Dream Software & Engineering Career",
+    desc: "Learn with real work experience and get guaranteed placement as a Full-Stack or Backend Developer at product-based companies.",
+    cover: "https://www.logopik.com/wp-content/uploads/edd/2018/10/software-development-banner-psd.png",
+  },
+  {
+    id: 2,
+    type: "admin",
+    date: "API. 25, 2022",
+    com: "5 COMMENTS ",
+    title: "My Journey of Python and Machine Learning",
+    desc: "This is designed for students aiming to start with Python and learn the depth of ML and Deep learning.In this course we start with the basics of Python programming ,OOPs in Python and various libs.",
+    cover: "https://www.wi6labs.com/wp-content/uploads/2019/12/Machine-learning-logo-1.png",
+  },
+  {
+    id: 3,
+    type: "user",
+    date: "MAY. 15, 2022",
+    com: "10 COMMENTS ",
+    title: "Foundation of Data Structure and Algorithms",
+    desc: "This is for anybody who has not coded at least 200hours of dsa . No prerequisites ,starts from scratch,and help you prepare for a very strong foundation for later on solving on leetcode,gfg .",
+    cover: "https://miro.medium.com/max/650/1*njvNP49632naRWy_DPxDbQ.png",
+  },
+  {
+    id: 4,
+    type: "admin",
+    date: "JAN. 02, 2022",
+    com: "3 COMMENTS ",
+    title: "Full Stack developer Journey",
+    desc: "A full stack web developer is a person who can develop both client and server software. In addition to mastering HTML and CSS, he/she also knows how to: Program a browser (like using JavaScript, jQuery, Angular, or Vue) Program a server (like using PHP, ASP, Python, or Node Work on professional projects like developers at AirBnB, Amazon, Netflix, with mentorship, to master full-stack development . with mentorship, to master Full-Stack skills in an actual developer environment.",
+    cover: "https://www.timetoact.de/TTA/Kompetenzen/Application%20Development/Fullstack%20Development/image-thumb__6508__Default/Grafik-Fullstack.webp",
+  },
+  {
+    id: 5,
+    type: "admin",
+    date: "DEC. 14, 2022",
+    com: "3 COMMENTS ",
+    title: "Backend Developer Career",
+    desc: "Build projects like developers at Zomato, Google, Zerodha, with mentorship, to learn backend development deeply. Backend Development is also known as server-side development. It is everything that the users don't see and contains behind-the-scenes activities that occur when performing any action on a website. It focuses primarily on databases, backend logic, APIs, and Servers. A backend developer is responsible for writing backend codes of any website and how is it going to communicate",
+    cover: "https://anteelo.com/wp-content/uploads/2021/05/back-end-developer-skills.jpg",
+  },
+  {
+    id: 6,
+    type: "user",
+    date: "JAN. 18, 2021",
+    com: "12 COMMENTS ",
+    title: "Right steps taken at right Times",
+    desc: "Learn Web Development by building 25 websites and mobile apps using HTML, CSS, Javascript, PHP, Python, MySQL & more! Best-selling Udemy instructor Rob Percival wants to revolutionize the way people learn to code by making it simple, logical, fun and, above all, accessible.  But as just one man, Rob couldn’t create all the courses his students - more than half a million of them - wanted. That interactive, and easy to understand.  ",
+    cover: "https://pbs.twimg.com/profile_images/1496388950436597760/kzYYo-jS_400x400.jpg",
+  },
+]
+export const testimonal = [
+  {
+    id: 1,
+    name: "Stephen Jonas",
+    post: "SYSTEM DESIGN ENGINEER",
+    desc: "Despite being from a tier 3 college, I would like to thank my mentors & teachers at EDL who helped me land a job at Microsoft. Both the teachers are brilliant teachers who know how to explain difficult concepts in an easy way.",
+    cover: "./images/testo/t1.webp",
+  },
+  {
+    id: 2,
+    name: "Shwan Accola",
+    post: "SENIOR SOFTWARE DEVELOPER",
+    desc: "The experience and knowledge I learned at EDL greatly sharpened my skills and allowed me to pass the HackerRank and technical interview rounds. The placement team coordinated everything perfectly and it was unbelievable to get an offer letter of 10 LPA. Grateful to EDL..",
+    cover: "./images/testo/t2.webp",
+  },
+  {
+    id: 3,
+    name: "Stalwart Saxena",
+    post: "AUTOMATION AND TEST ENGINEER at Accolite",
+    desc: "Thank you for providing great platform & carefully curated content for learning. Recently Accolite visited our campus and I was interviewed by Accolite and got the offer just because of EDL. Thanks a lot. Keep up the amazing work.",
+    cover: "./images/testo/t3.webp",
+  },
+]
